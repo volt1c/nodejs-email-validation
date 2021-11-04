949a358aedde639c67ac0278bcd05d0d6c668342
Agil-Dev <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-05 00:00:30 +0100

    feat: add validation conditions

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const pattern =
+const regexp =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const validate = (email) => {
-  const regexp = new RegExp(pattern)
+  let emailParts = email.split('@')
+
+  if (emailParts.length !== 2) return false
+
+  let [account, address] = emailParts
+
+  if (account.length > 64) return false
+  if (address.length > 255) return false
+
+  let domainParts = address.split('.')
+
+  if (domainParts.some((part) => part.length > 63)) return false
+
   if (!regexp.test(email)) return false
+
   return true
 }
 
